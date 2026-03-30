143690d3f94da8347f34fccf4c51ac83e7bb332a
Spicon861 <spicon861@gmail.com>
2026-03-30 15:31:56 -0500

    nuevo corte

---
+++ corte 1/Arrays y Ordenamiento/src/mascota.java
@@ -0,0 +1,11 @@
+public class mascota {
+    int historial;
+    String nombre;
+    int edad;
+    public mascota(int historial, String nombre, int edad) {
+        this.historial = historial;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    public mascota() {}  
+}
---
+++ corte 2/listas enlazadas/src/Ejercicio1/vagon.java
@@ -0,0 +1,7 @@
+public class vagon {
+    String contenido;
+    double persoToneladas;
+    String origen;
+    String destino;
+    Vagon siguiente;
+}
